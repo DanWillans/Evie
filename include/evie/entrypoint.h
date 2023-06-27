3245c3ab1b3976dd6918faa2535dfc32e674c988
Dan Willans <willansdan@googlemail.com>
2023-06-27 09:57:38 +0100

    Add main entrypoint for mac and ubuntu

--- include/evie/entrypoint.h
+++ include/evie/entrypoint.h
@@ -17,6 +17,17 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
   return 0;
 }
 
+#else
+
+extern std::unique_ptr<evie::Application> CreateApplication();
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
+{
+  auto app = CreateApplication();
+  app->Run();
+  return 0;
+}
+
 #endif
 
 #endif//  EVIE_ENTRYPOINT_H_
